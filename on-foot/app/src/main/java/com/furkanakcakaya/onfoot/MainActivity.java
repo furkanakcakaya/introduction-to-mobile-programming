d21ffafffd5d1cb81d38f92c3fcce5b66ff5dc06
Furkan Akcakaya <akcakayafurkan@yandex.com>
2022-05-19 11:59:09 +0300

    sensor change

--- on-foot/app/src/main/java/com/furkanakcakaya/onfoot/MainActivity.java
+++ on-foot/app/src/main/java/com/furkanakcakaya/onfoot/MainActivity.java
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private static final String TAG = "MainActivity";
     private SensorManager sensorManager;
     private Sensor linearAccelerationSensor;
-    private Sensor proximitySensor;
+    private Sensor lightSensor;
     private TextView tvResult;
     private Button btnNext;
     private static final double proximityE = 0.9;
     private static final double linearAccE = 0.055;
-    private boolean isProximityBelowThreshold = false;
+    private boolean isLightBelowThreshold = false;
     private boolean isLinearAccBelowThreshold = false;
 
     @Override
@@ -33,10 +33,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         linearAccelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        Log.d(TAG, "onCreate: " + proximitySensor.getResolution());
+        Log.d(TAG, "onCreate: " + lightSensor.getResolution());
         Log.d(TAG, "onCreate: " + linearAccelerationSensor.getResolution());
 
         tvResult = findViewById(R.id.tvResult);
@@ -46,7 +46,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         btnNext.setOnClickListener(view ->{
             Intent intent = new Intent(MainActivity.this, BroadcastActivity.class);
             startActivity(intent);
-
         });
 
     }
@@ -54,34 +53,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, proximitySensor,SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener( this, linearAccelerationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener( this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener( this);
+    protected void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this);
+        Log.i(TAG, "onDestroy: unregister listener");
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent)    {
-        if (sensorEvent.sensor.getType() == Sensor.TYPE_PROXIMITY) {
-            if (sensorEvent.values[0] < proximityE) {
-                isProximityBelowThreshold = true;
-            }else{
-                isProximityBelowThreshold = false;
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_LIGHT) {
+            if (sensorEvent.values[0] < proximityE && !isLightBelowThreshold) {
+                isLightBelowThreshold = true;
+                update();
+            }else if (sensorEvent.values[0] >= proximityE && isLightBelowThreshold) {
+                isLightBelowThreshold = false;
+                update();
             }
         }
         if (sensorEvent.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
             double x = Math.sqrt(sensorEvent.values[0] * sensorEvent.values[0] + sensorEvent.values[1] * sensorEvent.values[1] + sensorEvent.values[2] * sensorEvent.values[2]);
-            if (x < linearAccE) {
+            if (x < linearAccE && !isLinearAccBelowThreshold) {
                 isLinearAccBelowThreshold = true;
-            }else {
+                update();
+            }else if(x >= linearAccE && isLinearAccBelowThreshold){
                 isLinearAccBelowThreshold = false;
+                update();
             }
         }
-        update();
     }
 
     @Override
@@ -90,7 +93,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     void update(){
         Intent intent = new Intent();
         intent.setAction("com.furkanakcakaya.onfoot.UPDATE");
-        if (isProximityBelowThreshold) {
+        if (isLightBelowThreshold) {
             if (isLinearAccBelowThreshold) {
                 tvResult.setText("CEPTE, HAREKETSIZ");
                 intent.putExtra("mode", "default");
@@ -107,6 +110,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             tvResult.setText("CEPTE DEGIL, HAREKETLI");
             intent.putExtra("mode", "default");
         }
-        sendBroadcast(intent);
+        Log.i(TAG, "update: sending broadcast");
+        intent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        getApplicationContext().sendBroadcast(intent);
     }
 }
